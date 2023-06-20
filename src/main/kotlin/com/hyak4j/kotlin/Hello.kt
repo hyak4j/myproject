1b29e49d769b4ef89699dd584a6254bdb3015f2a
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-06-20 22:48:55 +0800

    Initial commit

---
+++ src/main/kotlin/com/hyak4j/kotlin/Hello.kt
@@ -0,0 +1,14 @@
+package com.hyak4j.kotlin
+
+fun main() {
+//    println("Hello Kotlin!")
+//    Person().hello()
+    val h = Person()
+    h.hello()
+}
+
+class Person{
+    fun hello(){
+        println("Hello Kotlin!!")
+    }
+}
