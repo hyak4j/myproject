c1bc1356f0d68337be8fb5f93f851b41e9d26f27
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-07-06 21:58:53 +0800

    Kotlin 寫法簡化改寫

--- src/main/kotlin/com/hyak4j/kotlin/student/Student.kt
+++ src/main/kotlin/com/hyak4j/kotlin/student/Student.kt
@@ -24,43 +24,28 @@ private fun userInput() {
 
 class Student(var name: String?, var english: Int, var math: Int) {
     fun print(){
-        print(name + "\t" + english + "\t" + math +
-                "\t" + getAverage() + "\t" +
-                if (getAverage() >= 60) "PASS" else "FAILED")
-        println("\t" + grading())
+        println("$name\t$english\t$math\t${getAverage()}\t${passOrFailed()}\t${grading()}")
     }
 
-    fun grading() :Char{
-        var grading = when(getAverage()) {
-            in 90..100 -> 'A'
-            in 80..89 -> 'B'
-            in 70..79 -> 'C'
-            in 60..69 -> 'D'
-            else -> 'F'
-        }
-        return grading
+    fun grading() = when(getAverage()){
+        in 90..100 -> 'A'
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
     }
 
-    fun getAverage() :Int{
-        return (english + math) / 2
+    fun passOrFailed() = if (getAverage() >= 60) "PASS" else "FAILED"
+
+    fun getAverage() = (english + math) / 2
+
+    fun highest() = if (english > math) {
+        println("English")
+        english
+    } else{
+        println("Math")
+        math
     }
 
-    fun highest() : Int {
-        var max = if (english > math) {
-            println("English")
-            english
-        } else{
-            println("Math")
-            math
-        }
-//        if (english > math){
-//            max = english
-//        }else{
-//            max = math
-//        }
-        return max
-    }
-    fun nameCheck(){
-        println(name?.length)
-    }
+    fun nameCheck() =  println(name?.length)
 }
